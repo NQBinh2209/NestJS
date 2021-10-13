9e78dafb46b48985185b6c499ae4f93178f70039
Nguyen <n.q.binh113@gmail.com>
2021-10-13 13:34:51 +0700

    origin

--- Config_Entity/src/user/status-user.model.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-export class StatusUser {
-  TRUE = 'TRUE';
-  FALSE = 'FALSE';
-}
--- Config_Entity/src/user/user.entity.ts
+++ Config_Entity/src/user/user.entity.ts
@@ -1,15 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { StatusUser } from './status-user.model';
 
-@Entity('USER')
-export class User extends BaseEntity {
+@Entity()
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
   username: string;
   @Column()
   password: string;
-  @Column()
-  status: StatusUser;
 }
